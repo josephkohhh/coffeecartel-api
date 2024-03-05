c440de52fd72c5938c9e0ae2e7360aa191910957
Joseph Koh <kohjoseph97@gmail.com>
2024-03-05 18:54:01 +0800

    code fix

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -6,31 +6,36 @@
 
 import { Sequelize } from "sequelize";
 
-// const databaseName = process.env.DATABASE_NAME;
-// const username = process.env.DATABASE_USERNAME;
-// const password = process.env.DATABASE_PASSWORD;
-// const host = process.env.DATABASE_HOST;
-const databaseURL = process.env.DATABASE_URL;
+const databaseName = process.env.DATABASE_NAME;
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const host = process.env.DATABASE_HOST;
+// const databaseURL = process.env.DATABASE_URL;
 
-// export const sequelize = new Sequelize(databaseName, username, password, {
-//   host: host, // Running on Render web svc
-//   dialect: "mysql",
-//   // pool: {
-//   //   max: 10,
-//   //   min: 0,
-//   //   acquire: 30000, // Max time to wait for a connection from the pool
-//   //   idle: 10000 // Max time that a connection can remain idle in the pool before being released
-//   // }
-// });
-
-export const sequelize = new Sequelize(databaseURL, {
+export const sequelize = new Sequelize(databaseName, username, password, {
+  host: host, // Running on Render web svc
+  dialect: "mysql",
   dialectOptions: {
     ssl: {
-      require: true,
-      rejectUnauthorized: true,
+      rejectUnauthorized: false,
     },
   },
+  // pool: {
+  //   max: 10,
+  //   min: 0,
+  //   acquire: 30000, // Max time to wait for a connection from the pool
+  //   idle: 10000 // Max time that a connection can remain idle in the pool before being released
+  // }
 });
+
+// export const sequelize = new Sequelize(databaseURL, {
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
 
 /*
  * Note: 
