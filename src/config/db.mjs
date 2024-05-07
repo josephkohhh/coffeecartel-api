04f3127e3625c99c128ec4e3a97c8f89e8254adf
Joseph Koh <kohjoseph97@gmail.com>
2024-05-07 19:44:22 +0800

    updated changes 7/5

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -10,10 +10,12 @@ const databaseName = process.env.DATABASE_NAME;
 const username = process.env.DATABASE_USERNAME;
 const password = process.env.DATABASE_PASSWORD;
 const host = process.env.DATABASE_HOST;
+const port = process.env.DATABASE_PORT;
 
 export const sequelize = new Sequelize(databaseName, username, password, {
   host: host, // Running on Render web svc
   dialect: "mysql",
+  port: port,
   // dialectOptions: { // Remove ssl connection
   //   ssl: {
   //     rejectUnauthorized: false,
